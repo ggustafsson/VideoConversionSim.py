66fb54c6c3c656afdb191a1fe57da5b551b6d81f
Göran Gustafsson <gustafsson.g@gmail.com>
2014-12-17 08:49:28 +0100

    Cleanup

--- SimResultsGroup.rb
+++ SimResultsGroup.rb
@@ -2,29 +2,9 @@
 
 # Written by Göran Gustafsson <gustafsson.g@gmail.com>.
 
-# Copyright (c) 2014, Göran Gustafsson. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#  Redistributions of source code must retain the above copyright notice, this
-#  list of conditions and the following disclaimer.
-#
-#  Redistributions in binary form must reproduce the above copyright notice,
-#  this list of conditions and the following disclaimer in the documentation
-#  and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+# Note that this is pretty ugly code that is specific for my needs. I use this
+# to create data that is later used with SimResultsHistogram.gpi to create the
+# specific histograms that i need for my project.
 
 filename = File.basename($0)
 
@@ -51,16 +31,7 @@ range8  = range1 * 8
 range9  = range1 * 9
 range10 = range1 * 10
 
-range1_value  = 0
-range2_value  = 0
-range3_value  = 0
-range4_value  = 0
-range5_value  = 0
-range6_value  = 0
-range7_value  = 0
-range8_value  = 0
-range9_value  = 0
-range10_value = 0
+range_values = Array.new(10, 0)
 
 data = CSV.read(file, { :headers => :first_row,
                         :col_sep => separator,
@@ -71,50 +42,50 @@ data.each do |row|
 
   case value
   when 0..range1-1 # Zero counts.
-    range1_value += 1
+    range_values[0] += 1
   when range1..range2-1
-    range2_value += 1
+    range_values[1] += 1
   when range2..range3-1
-    range3_value += 1
+    range_values[2] += 1
   when range3..range4-1
-    range4_value += 1
+    range_values[3] += 1
   when range4..range5-1
-    range5_value += 1
+    range_values[4] += 1
   when range5..range6-1
-    range6_value += 1
+    range_values[5] += 1
   when range6..range7-1
-    range7_value += 1
+    range_values[6] += 1
   when range7..range8-1
-    range8_value += 1
+    range_values[7] += 1
   when range8..range9-1
-    range9_value += 1
+    range_values[8] += 1
   when range9..range10 # Go up to 100.
-    range10_value += 1
+    range_values[9] += 1
   else
     puts "WARNING! Value '#{value}' skipped. Too low MAXVALUE!"
   end
 end
 
 if range10 > 100
-  puts "0-#{(range1 - 1) / 60} #{range1_value}"
-  puts "#{range1 / 60}-#{(range2 - 1) / 60} #{range2_value}"
-  puts "#{range2 / 60}-#{(range3 - 1) / 60} #{range3_value}"
-  puts "#{range3 / 60}-#{(range4 - 1) / 60} #{range4_value}"
-  puts "#{range4 / 60}-#{(range5 - 1) / 60} #{range5_value}"
-  puts "#{range5 / 60}-#{(range6 - 1) / 60} #{range6_value}"
-  puts "#{range6 / 60}-#{(range7 - 1) / 60} #{range7_value}"
-  puts "#{range7 / 60}-#{(range8 - 1) / 60} #{range8_value}"
-  puts "#{range8 / 60}-#{(range9 - 1) / 60} #{range9_value}"
-  puts "#{range9 / 60}-#{range10 / 60} #{range10_value}"
+  puts "0-#{(range1 - 1) / 60} #{range_values[0]}"
+  puts "#{range1 / 60}-#{(range2 - 1) / 60} #{range_values[1]}"
+  puts "#{range2 / 60}-#{(range3 - 1) / 60} #{range_values[2]}"
+  puts "#{range3 / 60}-#{(range4 - 1) / 60} #{range_values[3]}"
+  puts "#{range4 / 60}-#{(range5 - 1) / 60} #{range_values[4]}"
+  puts "#{range5 / 60}-#{(range6 - 1) / 60} #{range_values[5]}"
+  puts "#{range6 / 60}-#{(range7 - 1) / 60} #{range_values[6]}"
+  puts "#{range7 / 60}-#{(range8 - 1) / 60} #{range_values[7]}"
+  puts "#{range8 / 60}-#{(range9 - 1) / 60} #{range_values[8]}"
+  puts "#{range9 / 60}-#{range10 / 60} #{range_values[9]}"
 else
-  puts "0-#{range1 - 1} #{range1_value}"
-  puts "#{range1}-#{range2 - 1} #{range2_value}"
-  puts "#{range2}-#{range3 - 1} #{range3_value}"
-  puts "#{range3}-#{range4 - 1} #{range4_value}"
-  puts "#{range4}-#{range5 - 1} #{range5_value}"
-  puts "#{range5}-#{range6 - 1} #{range6_value}"
-  puts "#{range6}-#{range7 - 1} #{range7_value}"
-  puts "#{range7}-#{range8 - 1} #{range8_value}"
-  puts "#{range8}-#{range9 - 1} #{range9_value}"
-  puts "#{range9}-#{range10} #{range10_value}"
+  puts "0-#{range1 - 1} #{range_values[0]}"
+  puts "#{range1}-#{range2 - 1} #{range_values[1]}"
+  puts "#{range2}-#{range3 - 1} #{range_values[2]}"
+  puts "#{range3}-#{range4 - 1} #{range_values[3]}"
+  puts "#{range4}-#{range5 - 1} #{range_values[4]}"
+  puts "#{range5}-#{range6 - 1} #{range_values[5]}"
+  puts "#{range6}-#{range7 - 1} #{range_values[6]}"
+  puts "#{range7}-#{range8 - 1} #{range_values[7]}"
+  puts "#{range8}-#{range9 - 1} #{range_values[8]}"
+  puts "#{range9}-#{range10} #{range_values[9]}"
 end
