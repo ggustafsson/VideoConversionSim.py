9835cf7cfdabc4707151208777df98cb75e9f112
Göran Gustafsson <gustafsson.g@gmail.com>
2014-12-18 16:15:00 +0100

    Histogram enhancements

--- SimResultsGroup-Runner.zsh
+++ SimResultsGroup-Runner.zsh
@@ -6,10 +6,12 @@ cd Data
 
 ../SimResultsGroup.rb Results-8R.csv  1 2958 >| Results-8R-Mean.txt
 ../SimResultsGroup.rb Results-12R.csv 1 2958 >| Results-12R-Mean.txt
+../SimResultsGroup.rb Results-12R.csv 1 24   >| Results-12R-Mean-Alone.txt
 
 ../SimResultsGroup.rb Results-8R.csv  2 5694 >| Results-8R-Longest.txt
 ../SimResultsGroup.rb Results-12R.csv 2 5694 >| Results-12R-Longest.txt
+../SimResultsGroup.rb Results-12R.csv 2 688  >| Results-12R-Longest-Alone.txt
 
-../SimResultsGroup.rb Results-4R.csv  3 1440 >| Results-4R-Above.txt
 ../SimResultsGroup.rb Results-8R.csv  3 1440 >| Results-8R-Above.txt
 ../SimResultsGroup.rb Results-12R.csv 3 1440 >| Results-12R-Above.txt
+../SimResultsGroup.rb Results-12R.csv 3 45   >| Results-12R-Above-Alone.txt
